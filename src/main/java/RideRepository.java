5f3ba33fd183dab76a6ecf04080e3f2a3c990d25
DYSTAR14 <ydeepak141097@gmail.com>
2021-09-28 08:15:12 +0530

    Added: Dependence and remove comment code;

--- src/main/java/RideRepository.java
+++ src/main/java/RideRepository.java
@@ -6,45 +6,6 @@ public class RideRepository {
         this.rideRepositoryList = new HashMap<>();
     }
 
-//    public void dataEntryOfCabRide() {
-//        ride = new Ride(2.0, 5, CabRide.NORMAL);
-//        list = new ArrayList<>();
-//        list.add(ride);
-//        rideRepositoryList.put(11, list);
-//
-//        ride = new Ride(5.0, 4, CabRide.PREMIUM);
-//        list = new ArrayList<>();
-//        list.add(ride);
-//        rideRepositoryList.put(12, list);
-//
-//        ride = new Ride(0.1, 1, CabRide.PREMIUM);
-//        list = new ArrayList<>();
-//        list = rideRepositoryList.get(11);
-//        list.add(ride);
-//        rideRepositoryList.put(11, list);
-//
-//        ride = new Ride(2.0, 8, CabRide.NORMAL);
-//        list = new ArrayList<>();
-//        list.add(ride);
-//        rideRepositoryList.put(13, list);
-//
-//        ride = new Ride(8.0, 6, CabRide.PREMIUM);
-//        list = new ArrayList<>();
-//        list.add(ride);
-//        rideRepositoryList.put(14, list);
-//
-//        ride = new Ride(7.0, 8, CabRide.NORMAL);
-//        list = new ArrayList<>();
-//        list = rideRepositoryList.get(13);
-//        list.add(ride);
-//        rideRepositoryList.put(13, list);
-//    }
-
-//    public List<Ride> getUserRidesInvoice() {
-//        list = rideRepositoryList.get(this.id);
-//        return list;
-//    }
-
     public void addRides(String userId, Ride[] rides) {
         ArrayList<Ride> rideList = this.rideRepositoryList.get(userId);
         if(rideList != null)
